047e9b37513765ace89c31d545abcbac7edefe50
Hubert.Karw <hubert_karwowski@o2.pl>
2025-04-03 22:36:43 +0200

    java/medical-clinic/med11/validation patient validator class and implementation

--- src/main/java/com/HubertKarw/medical_clinic/Controller/PatientController.java
+++ src/main/java/com/HubertKarw/medical_clinic/Controller/PatientController.java
@@ -16,27 +16,27 @@ public class PatientController {
     private final PatientService patientService;
 
     @GetMapping
-    public List<Patient> viewPatients(){
-        return patientService.viewPatients();
+    public List<Patient> getPatients() {
+        return patientService.getPatients();
     }
 
     @GetMapping("/{email}")
-    public Patient getPatient(@PathVariable("email") String email){
+    public Patient getPatient(@PathVariable("email") String email) {
         return patientService.getPatient(email);
     }
 
     @PostMapping
-    public Patient addPatient(@RequestBody Patient patient){
+    public Patient addPatient(@RequestBody Patient patient) {
         return patientService.addPatient(patient);
     }
 
     @DeleteMapping("/{email}")
-    public void removePatient(@PathVariable("email") String email){
+    public void removePatient(@PathVariable("email") String email) {
         patientService.removePatient(email);
     }
 
     @PutMapping("/{email}")
-    public Patient modifyPatient(@PathVariable("email") String email, @RequestBody Patient patient){
-        return patientService.modifyPatient(email,patient);
+    public Patient modifyPatient(@PathVariable("email") String email, @RequestBody Patient patient) {
+        return patientService.modifyPatient(email, patient);
     }
 }
--- src/main/java/com/HubertKarw/medical_clinic/Repository/PatientRepository.java
+++ src/main/java/com/HubertKarw/medical_clinic/Repository/PatientRepository.java
@@ -1,9 +1,11 @@
 package com.HubertKarw.medical_clinic.Repository;
 
 import com.HubertKarw.medical_clinic.Model.Patient;
+import com.HubertKarw.medical_clinic.Validation.PatientValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,35 +15,38 @@ public class PatientRepository {
 
     private final List<Patient> patients;
 
-    public List<Patient> viewPatients(){
-        return patients;
+    public List<Patient> getPatients() {
+        return new ArrayList<>(patients);
     }
 
-    public Optional<Patient> findByEmail(String email){
+    public Optional<Patient> findByEmail(String email) {
         return patients.stream()
                 .filter(patient -> patient.getEmail().equalsIgnoreCase(email))
                 .findFirst();
     }
 
-    public Patient addPatient(Patient patient){
+    public Patient addPatient(Patient patient) {
+        PatientValidator.validatePatientCreation(patient);
+        PatientValidator.validateAddingPatient(new ArrayList<>(patients), patient.getEmail());
         patients.add(patient);
         return patient;
     }
-    public void removePatient(String email){
-        Patient p = patients.stream()
-                .filter(patient -> patient.getEmail().equalsIgnoreCase(email))
+
+    public void removePatient(String email) {
+        Patient patient = patients.stream()
+                .filter(patient1 -> patient1.getEmail().equalsIgnoreCase(email))
                 .findFirst()
-                .orElseThrow(()-> new IllegalArgumentException("User with tis eMail address does not exist"));
-        patients.remove(p);
+                .orElseThrow(() -> new IllegalArgumentException("User with tis eMail address does not exist"));
+        patients.remove(patient);
     }
-    public Patient modifyPatient(String email, Patient patient){
-        Patient p = patients.stream()
+
+    public Patient modifyPatient(String email, Patient newPatient) {
+        Patient patient = patients.stream()
                 .filter(pat -> pat.getEmail().equalsIgnoreCase(email))
                 .findFirst()
-                .orElseThrow(()-> new IllegalArgumentException("User with tis eMail address does not exist"));
-        patients.remove(p);
-        patients.add(patient);
-        return p;
-
+                .orElseThrow(() -> new IllegalArgumentException("User with tis eMail address does not exist"));
+        PatientValidator.validatePatientCreation(patient);
+        PatientValidator.validatePatientUpdate(patient, newPatient);
+        return patient.update(newPatient);
     }
 }
