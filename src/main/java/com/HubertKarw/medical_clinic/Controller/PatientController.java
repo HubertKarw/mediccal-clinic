edb2de0f8b9feb322ed135a4191f0e313d8716d4
Hubert.Karw <hubert_karwowski@o2.pl>
2025-04-14 23:08:26 +0200

    java/medical-clinic/med08/ Swagger implementation

--- src/main/java/com/HubertKarw/medical_clinic/Controller/PatientController.java
+++ src/main/java/com/HubertKarw/medical_clinic/Controller/PatientController.java
@@ -7,7 +7,15 @@ import com.HubertKarw.medical_clinic.Model.PatientDTO;
 import com.HubertKarw.medical_clinic.Service.PatientMapper;
 import com.HubertKarw.medical_clinic.Service.PatientService;
 import com.HubertKarw.medical_clinic.Service.PatientStructMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,42 +24,89 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Patient", description = "Patient operations")
 @RequestMapping("/patients")
 public class PatientController {
 
     private final PatientService patientService;
     private final PatientStructMapper mapper;
+
+    @Operation(summary = "Get all patients")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found patients", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = PatientDTO.class))})})
     @GetMapping
     public List<PatientDTO> getPatients() {
         return patientService.getPatients().stream()
                 .map(mapper::mapToDTO)
                 .collect(Collectors.toList());
     }
-
+    @Operation(summary = "Get patient by email")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found patient", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Patient not found", content = @Content)
+    })
     @GetMapping("/{email}")
     public PatientDTO getPatient(@PathVariable("email") String email) {
         return mapper.mapToDTO(patientService.getPatient(email));
     }
-
+    @Operation(summary = "create patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Created patient", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "400", description = "Patient is not valid", content = @Content)
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public PatientDTO addPatient(@RequestBody CreatePatientCommand command) {
+    public PatientDTO addPatient(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "Patient to create", required = true,
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CreatePatientCommand.class),
+                    examples = @ExampleObject(value = "{ \"Email\": \"email\", \"User\": \"User\", \"idCardNO\": \"1234\"" +
+                            ", \"firstName\": \"John\", \"lastName\": \"Doe\",  \"phoneNumber\": \"123456789\",  \"birthday\": \"1999-01-01\"}")))
+            @RequestBody CreatePatientCommand command) {
         return mapper.mapToDTO(patientService.addPatient(mapper.mapToPatient(command)));
     }
-
+    @Operation(summary = "Remove patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "removed patient", content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Patient not found", content = @Content)
+    })
     @DeleteMapping("/{email}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removePatient(@PathVariable("email") String email) {
         patientService.removePatient(email);
     }
-
+    @Operation(summary = "edit patient")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Edited patient information", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Patient not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Patient is not valid", content = @Content)
+    })
     @PutMapping("/{email}")
-    public PatientDTO modifyPatient(@PathVariable("email") String email, @RequestBody CreatePatientCommand patient) {
+    public PatientDTO modifyPatient(@PathVariable("email") String email,
+                                    @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                            description = "Patient to create", required = true,
+                                            content = @Content(mediaType = "application/json",
+                                                    schema = @Schema(implementation = CreatePatientCommand.class),
+                                                    examples = @ExampleObject(value = "{ \"Email\": \"email\", \"User\": \"User\", \"idCardNO\": \"1234\"" +
+                                                            ", \"firstName\": \"John\", \"lastName\": \"Doe\",  \"phoneNumber\": \"123456789\",  \"birthday\": \"1999-01-01\"}")))
+                                    @RequestBody CreatePatientCommand patient) {
         return mapper.mapToDTO(patientService.modifyPatient(email, mapper.mapToPatient(patient)));
     }
-
+    @Operation(summary = "edit patients password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Edited patient password", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = PatientDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "Patient not found", content = @Content),
+            @ApiResponse(responseCode = "400", description = "Patient is not valid", content = @Content)
+    })
     @PatchMapping("/{email}")
-    public PatientDTO setPassword(@PathVariable("email") String email, @RequestBody Password password) {
+    public PatientDTO setPassword(@PathVariable("email") String email,
+                                  @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                          description = "Patient to create", required = true,
+                                          content = @Content(mediaType = "application/json",
+                                                  schema = @Schema(implementation = CreatePatientCommand.class),
+                                                  examples = @ExampleObject(value = "{ \"Email\": \"email\", \"User\": \"User\", \"idCardNO\": \"1234\"" +
+                                                          ", \"firstName\": \"John\", \"lastName\": \"Doe\",  \"phoneNumber\": \"123456789\",  \"birthday\": \"1999-01-01\"}")))
+                                  @RequestBody Password password) {
         return mapper.mapToDTO(patientService.setPassword(email, password));
     }
 }
--- src/main/java/com/HubertKarw/medical_clinic/Controller/UserController.java
+++ src/main/java/com/HubertKarw/medical_clinic/Controller/UserController.java
@@ -1,9 +1,18 @@
 package com.HubertKarw.medical_clinic.Controller;
 
+import com.HubertKarw.medical_clinic.Model.CreatePatientCommand;
 import com.HubertKarw.medical_clinic.Model.CreateUserCommand;
+import com.HubertKarw.medical_clinic.Model.PatientDTO;
 import com.HubertKarw.medical_clinic.Model.UserDTO;
 import com.HubertKarw.medical_clinic.Service.UserService;
 import com.HubertKarw.medical_clinic.Service.UserStructMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
@@ -15,11 +24,16 @@ import java.util.stream.Collectors;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
+@Tag(name = "User", description = "User operations")
 public class UserController {
 
     private final UserService userService;
     private final UserStructMapper mapper;
-
+    @Operation(summary = "Get all Users")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found User",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserDTO.class))})})
     @GetMapping
     public List<UserDTO> getUsers(){
         return userService.getUsers()
@@ -27,24 +41,52 @@ public class UserController {
                 .map(mapper::toDto)
                 .collect(Collectors.toList());
     }
-
+    @Operation(summary = "Get User by username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found user", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = UserDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content)
+    })
     @GetMapping("/{username}")
     public  UserDTO getUser(@PathVariable("username") String username){
         return mapper.toDto(userService.getUser(username));
     }
-
+    @Operation(summary = "Create user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "created user", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = UserDTO.class))}),
+    })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public UserDTO addUser(@RequestBody CreateUserCommand command){
+    public UserDTO addUser(@io.swagger.v3.oas.annotations.parameters.RequestBody(
+            description = "User to create", required = true,
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = CreateUserCommand.class),
+                    examples = @ExampleObject(value = "{ \"username\": \"user123\", \"password\": \"password\"}")))
+            @RequestBody CreateUserCommand command){
         return mapper.toDto(userService.addUser(mapper.toUser(command)));
     }
+    @Operation(summary = "delete User by username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found user", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = UserDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content)
+    })
     @DeleteMapping("/{username}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeUser(@PathVariable("username") String username){
         userService.removeUser(username);
     }
+    @Operation(summary = "modify User by username")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found user", content = {@Content(mediaType = "application/json",schema = @Schema(implementation = UserDTO.class))}),
+            @ApiResponse(responseCode = "404", description = "User not found", content = @Content)
+    })
     @PutMapping("/{username}")
-    public UserDTO modifyUser(@PathVariable("username")String username, @RequestBody CreateUserCommand command){
+    public UserDTO modifyUser(@PathVariable("username")String username,
+                              @io.swagger.v3.oas.annotations.parameters.RequestBody(
+                                      description = "modified user", required = true,
+                                      content = @Content(mediaType = "application/json",
+                                              schema = @Schema(implementation = CreateUserCommand.class),
+                                              examples = @ExampleObject(value = "{ \"username\": \"user123\", \"password\": \"password\"}")))
+                              @RequestBody CreateUserCommand command){
         return mapper.toDto(userService.modifyUser(username,mapper.toUser(command)));
     }
 }
