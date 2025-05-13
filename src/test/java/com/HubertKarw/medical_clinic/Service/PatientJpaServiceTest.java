571cba728d37ed0c2169bea2ef226a256bfc9036
Hubert.Karw <hubert_karwowski@o2.pl>
2025-05-13 18:09:37 +0200

    java/medical-clinic/liquibase/liquibase fix

--- src/test/java/com/HubertKarw/medical_clinic/Service/PatientJpaServiceTest.java
+++ src/test/java/com/HubertKarw/medical_clinic/Service/PatientJpaServiceTest.java
@@ -14,8 +14,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.any;
 
@@ -58,5 +57,43 @@ public class PatientJpaServiceTest {
         Patient result = patientJpaService.getPatient("x1");
         //then
         assertEquals(patient.getId(),result.getId());
+    }
+
+    @Test
+    void addPatient_validPatient_patientAdded(){
+        //given
+        User user = new User(1l, "123", "321");
+        Patient patient = new Patient(null, "x1", user, "123", "xyz", "xzy", "123", LocalDate.of(1999, 11, 11));
+        Patient patientSaved = new Patient(123L, "x1", user, "123", "xyz", "xzy", "123", LocalDate.of(1999, 11, 11));
+        when(patientJpaRepository.save(any())).thenReturn(patientSaved);
+        //when
+        Patient result = patientJpaService.addPatient(patient);
+        //then
+        assertEquals(patient.getEmail(),patientSaved.getEmail());
+    }
+    @Test
+    void removePatient_patientExists_patientRemoved(){
+        //given
+        User user = new User(1l, "123", "321");
+        Patient patient = new Patient(null, "x1", user, "123", "xyz", "xzy", "123", LocalDate.of(1999, 11, 11));
+        Patient patientSaved = new Patient(123L, "x1", user, "123", "xyz", "xzy", "123", LocalDate.of(1999, 11, 11));
+        when(patientJpaRepository.findByEmail(any())).thenReturn(Optional.of(patient));
+        //when
+        patientJpaService.removePatient(patient.getEmail());
+        //then
+        assertEquals(1,1);
+    }
+    @Test
+    void modifyPatient_patientExists_patientModified(){
+        User user = new User(1l, "123", "321");
+        Patient patient = new Patient(null, "x1", user, "123", "xyz", "xzy", "123", LocalDate.of(1999, 11, 11));
+        Patient modifiedPatient = new Patient(123L, "x1", user, "123", "xyzz", "xzy", "123", LocalDate.of(1999, 11, 11));
+        when(patientJpaRepository.findByEmail(any())).thenReturn(Optional.of(patient));
+        when(patient.update(any(Patient.class))).thenReturn(modifiedPatient);
+        when(patientJpaRepository.save(any())).thenReturn(modifiedPatient);
+        //when
+        patientJpaService.modifyPatient("x1",modifiedPatient);
+        //then
+        assertNotEquals(patient.getFirstName(),modifiedPatient.getFirstName());
     }
 }
